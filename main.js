bc7678a4d7643ae1adf0e3d5c0f058697aab6ebd
jaberphpdev <jaberphpdev@gmail.com>
2021-12-27 23:24:19 +0600

    2nd lesson done with task

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data(){
         return{
             product: 'Socks',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos vel quas molestias! Omnis voluptate, quas ea dolore dolor commodi, obcaecati deleniti sapiente mollitia, minima ipsam tempora rerum nemo nobis cupiditate.'
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos vel quas molestias! Omnis voluptate, quas ea dolore dolor commodi, obcaecati deleniti sapiente mollitia, minima ipsam tempora rerum nemo nobis cupiditate.',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://github.com/JaberWiki/intro-to-vue-3',
         }
     }
 })
