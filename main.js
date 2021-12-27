30ee101778e17416e45bda8dc9e8ca347d850a45
jaberphpdev <jaberphpdev@gmail.com>
2021-12-27 23:10:30 +0600

    First lesson done with task

---
+++ main.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            product: 'Socks',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos vel quas molestias! Omnis voluptate, quas ea dolore dolor commodi, obcaecati deleniti sapiente mollitia, minima ipsam tempora rerum nemo nobis cupiditate.'
+        }
+    }
+})
